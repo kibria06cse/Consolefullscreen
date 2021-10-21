bd45ae12d1d2b55300c9e2a55c06297b38cae59b
Abdus Salam Azad <azad.kibria@streamstech.com>
2021-10-22 00:21:31 +0600

    Add project files.

---
+++ Consolefullscreen/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Consolefullscreen
+{
+    class Program
+    {
+        [DllImport("kernel32.dll", ExactSpelling = true)]
+        private static extern IntPtr GetConsoleWindow();
+        private static IntPtr ThisConsole = GetConsoleWindow();
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+        private const int MAXIMIZE = 3;
+
+        static void Main(string[] args)
+        {
+            var isFullScreen = CheckIfFullScreen();
+            if (isFullScreen)
+                Console.WriteLine("You screen is full screen mode.");
+            else
+                Console.WriteLine("You screen is not full screen mode.");
+            Console.SetWindowSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
+
+            ShowWindow(ThisConsole, MAXIMIZE);
+            Console.ReadLine();
+        }
+
+        private static bool CheckIfFullScreen()
+        {
+            var windowHeight = Console.WindowHeight;
+            var windowWidth = Console.WindowWidth;
+
+            if (windowWidth == Console.LargestWindowWidth && windowHeight == Console.LargestWindowHeight)
+                return true;
+
+            return false;
+        }
+    }
+}
